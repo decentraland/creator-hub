577db0f4c2f566a6d56ae9249b8b588ccd04fbcb
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-07-05 14:12:22 -0300

    move hoc init to renderer/src/index.ts

--- packages/renderer/src/components/App/component.tsx
+++ packages/renderer/src/components/App/component.tsx
@@ -1,23 +1,13 @@
 import {useEffect} from 'react';
-import {Provider as StoreProvider} from 'react-redux';
-import {MemoryRouter as Router, Routes, Route} from 'react-router-dom';
-import {ThemeProvider} from '@mui/material/styles';
-import {dark} from 'decentraland-ui2/dist/theme';
-
-import {store, useDispatch, useSelector} from '#store';
-import {TranslationProvider} from '/@/components/TranslationProvider';
-import {fetchTranslations} from '/@/modules/store/reducers/translation';
-import {locales} from '/@/modules/store/reducers/translation/utils';
+import {useDispatch, useSelector} from '#store';
 import {getWorkspace} from '/@/modules/store/reducers/workspace/thunks';
 
 import {ScenesPage} from '/@/components/ScenesPage';
 import {SortBy} from '/@/components/ScenesPage/types';
 
-import '/@/themes';
-
 const noop = () => undefined;
 
-function Root() {
+export function App() {
   const dispatch = useDispatch();
   const workspace = useSelector(state => state.workspace);
 
@@ -34,27 +24,5 @@ function Root() {
         onSort={noop}
       />
     </div>
-  );
-}
-
-export function App() {
-  return (
-    <StoreProvider store={store}>
-      <TranslationProvider
-        locales={locales}
-        fetchTranslations={fetchTranslations}
-      >
-        <ThemeProvider theme={dark}>
-          <Router>
-            <Routes>
-              <Route
-                path="/"
-                element={<Root />}
-              />
-            </Routes>
-          </Router>
-        </ThemeProvider>
-      </TranslationProvider>
-    </StoreProvider>
   );
 }
--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -1,12 +1,40 @@
 import React from 'react';
 import {createRoot} from 'react-dom/client';
+import {Provider as StoreProvider} from 'react-redux';
+import {MemoryRouter as Router, Routes, Route} from 'react-router-dom';
+import {ThemeProvider} from '@mui/material/styles';
+import {dark} from 'decentraland-ui2/dist/theme';
+
+import {store} from '#store';
+import {TranslationProvider} from '/@/components/TranslationProvider';
+import {fetchTranslations} from '/@/modules/store/reducers/translation';
+import {locales} from '/@/modules/store/reducers/translation/utils';
 
 import {App} from '/@/components/App/component';
 
+import '/@/themes';
+
 const container = document.getElementById('app')!;
 const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider store={store}>
+      <TranslationProvider
+        locales={locales}
+        fetchTranslations={fetchTranslations}
+      >
+        <ThemeProvider theme={dark}>
+          <Router>
+            <Routes>
+              <Route
+                path="/"
+                element={<App />}
+              />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </TranslationProvider>
+    </StoreProvider>
   </React.StrictMode>,
 );
