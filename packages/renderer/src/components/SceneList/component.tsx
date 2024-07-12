144789be7bd70348895d0df37ce9db626ac1c13c
Juan Cazala <juancazala@gmail.com>
2024-07-12 17:36:44 -0300

    fix: no scenes

--- packages/renderer/src/components/SceneList/component.tsx
+++ packages/renderer/src/components/SceneList/component.tsx
@@ -133,7 +133,7 @@ export function SceneList({ projects, sortBy, onSort }: Props) {
       </Column>
       <Box
         display="grid"
-        gridTemplateColumns="repeat(4, 1fr)"
+        // gridTemplateColumns="repeat(4, 1fr)"
         gap={2}
       >
         {renderProjects()}
