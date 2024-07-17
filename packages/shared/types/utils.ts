63664e20cb05d3e5c6f51ee8ba768485d8f3b69d
Nicolas Echezarreta <nicoecheza@gmail.com>
2024-07-17 19:16:38 -0300

    fix comments

--- packages/shared/types/utils.ts
+++ packages/shared/types/utils.ts
@@ -1,8 +1,10 @@
 export type Tail<T extends any[]> = T extends [any, ...infer Rest] ? Rest : never;
 
 export function isUrl(url: string) {
-  const regex = new RegExp(
-    /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/gm,
-  );
-  return regex.test(url);
+  try {
+    new URL(url);
+    return true;
+  } catch (_error) {
+    return false;
+  }
 }
