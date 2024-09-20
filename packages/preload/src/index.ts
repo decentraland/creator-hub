7bff38da50eec0c2edfe31fc2982c86b973bd411
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-09-20 18:46:56 -0300

    feat: Add logic to save settings in the config file

--- packages/preload/src/index.ts
+++ packages/preload/src/index.ts
@@ -4,3 +4,4 @@ export * as workspace from './modules/workspace';
 export * as fs from './modules/fs';
 export * as analytics from './modules/analytics';
 export * as npm from './modules/npm';
+export * as settings from './modules/settings';
