8a0d161839e56b48bed6beaaa0c9b762e8b8f62c
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-09-20 18:46:56 -0300

    feat: Add logic to save settings in the config file

---
+++ packages/preload/src/modules/settings.ts
@@ -0,0 +1,35 @@
+import { UPDATE_DEPENDENCIES_STRATEGY } from '/shared/types/settings';
+import { invoke } from './invoke';
+import { getConfig, setConfig } from './config';
+
+export async function getScenesPath() {
+  const config = await getConfig();
+  return config.scenesPath ?? (await invoke('electron.getAppHome'));
+}
+
+export async function setScenesPath(path: string) {
+  await setConfig(config => (config.scenesPath = path));
+}
+
+// Helper to check if a value is part of the enum
+function isValidUpdateStrategy(value: string): value is UPDATE_DEPENDENCIES_STRATEGY {
+  return Object.values(UPDATE_DEPENDENCIES_STRATEGY).includes(
+    value as UPDATE_DEPENDENCIES_STRATEGY,
+  );
+}
+
+export async function getUpdateDependenciesStrategy() {
+  const config = await getConfig();
+  if (
+    config.updateDependenciesStrategy &&
+    isValidUpdateStrategy(config.updateDependenciesStrategy)
+  ) {
+    return config.updateDependenciesStrategy;
+  } else {
+    return UPDATE_DEPENDENCIES_STRATEGY.NOTIFY;
+  }
+}
+
+export async function setUpdateDependenciesStrategy(strategy: UPDATE_DEPENDENCIES_STRATEGY) {
+  await setConfig(config => (config.updateDependenciesStrategy = strategy));
+}
