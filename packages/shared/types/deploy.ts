c98d6d52046dd70b55e1366550af1e0ee1783fb9
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-10-28 23:37:34 -0300

    feat: Add types

--- packages/shared/types/deploy.ts
+++ packages/shared/types/deploy.ts
@@ -2,4 +2,6 @@ export const DEPLOY_URLS = {
   WORLDS: 'https://worlds-content-server.decentraland.org',
   TEST: 'https://peer-testing.decentraland.org',
   DEV_WORLDS: 'https://worlds-content-server.decentraland.zone',
+  CATALYST_SERVER: 'https://peer.decentraland.org',
+  DEV_CATALYST_SERVER: 'https://peer.decentraland.zone',
 };
