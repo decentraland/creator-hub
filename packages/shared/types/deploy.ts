8930bd3f6afe66c798a50a41ac2afb797cbd0f46
Nicolas Echezarreta <nicoecheza@gmail.com>
2025-01-17 10:30:11 -0300

    fix registry URL

--- packages/shared/types/deploy.ts
+++ packages/shared/types/deploy.ts
@@ -4,4 +4,6 @@ export const DEPLOY_URLS = {
   DEV_WORLDS: 'https://worlds-content-server.decentraland.zone',
   CATALYST_SERVER: 'https://peer.decentraland.org',
   DEV_CATALYST_SERVER: 'https://peer.decentraland.zone',
+  ASSET_BUNDLE_REGISTRY: 'https://asset-bundle-registry.decentraland.org',
+  DEV_ASSET_BUNDLE_REGISTRY: 'https://asset-bundle-registry.decentraland.zone',
 };
