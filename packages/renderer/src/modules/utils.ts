80c4ee3e99225025241c33457242764295c34a74
Ignacio Mazzara <nachomazzara@gmail.com>
2025-03-07 16:34:41 -0300

    chore: linter

--- packages/renderer/src/modules/utils.ts
+++ packages/renderer/src/modules/utils.ts
@@ -1,7 +1,7 @@
 import { ChainId } from '@dcl/schemas/dist/dapps/chain-id';
 
 export function isDev(chainId: ChainId): boolean {
-  return chainId === ChainId.ETHEREUM_SEPOLIA
+  return chainId === ChainId.ETHEREUM_SEPOLIA;
 }
 
 export function getCannyURL() {
