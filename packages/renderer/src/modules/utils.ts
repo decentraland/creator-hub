03f573c4d74f8a8e8d5fc9ac218296a8ffdebe52
Ignacio Mazzara <nachomazzara@gmail.com>
2025-03-07 17:11:50 -0300

    chore: 👮🏻‍♂️

--- packages/renderer/src/modules/utils.ts
+++ packages/renderer/src/modules/utils.ts
@@ -1,13 +1,13 @@
-import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
+import { ChainId } from '@dcl/schemas/dist/dapps/chain-id';
 
 export function isDev(chainId: ChainId): boolean {
-  return chainId === ChainId.ETHEREUM_SEPOLIA
+  return chainId === ChainId.ETHEREUM_SEPOLIA;
 }
 
 export function getReportIssuesURL() {
-  return 'https://decentraland.canny.io/creator-hub'
+  return 'https://decentraland.canny.io/creator-hub';
 }
 
 export function getFeedbackURL() {
-  return 'https://decentraland.typeform.com/creatorhub'
+  return 'https://decentraland.typeform.com/creatorhub';
 }
