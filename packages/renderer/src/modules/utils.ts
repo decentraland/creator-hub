e387e4f5d28032c6e991fe73e655f5786f803858
Ignacio Mazzara <nachomazzara@gmail.com>
2025-03-07 16:27:21 -0300

    chore: linter

--- packages/renderer/src/modules/utils.ts
+++ packages/renderer/src/modules/utils.ts
@@ -1,9 +1,9 @@
-import { ChainId } from '@dcl/schemas/dist/dapps/chain-id'
+import { ChainId } from '@dcl/schemas/dist/dapps/chain-id';
 
 export function isDev(chainId: ChainId): boolean {
   return chainId === ChainId.ETHEREUM_SEPOLIA
 }
 
 export function getCannyURL() {
-  return 'https://decentraland.canny.io/creator-hub'
+  return 'https://decentraland.canny.io/creator-hub';
 }
