320fe1302e4c62e534c8336f9308b6c7298c0e90
Nicolas Echezarreta <nicoecheza@gmail.com>
2025-04-10 09:29:21 -0300

    move to Applications folder when necessary

--- packages/renderer/src/components/Install/component.tsx
+++ packages/renderer/src/components/Install/component.tsx
@@ -1,12 +1,17 @@
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { CircularProgress as Loader } from 'decentraland-ui2';
-import { useEditor } from '/@/hooks/useEditor';
+import { CircularProgress as Loader, Typography } from 'decentraland-ui2';
+
 import { t } from '/@/modules/store/translation/utils';
+
+import { useEditor } from '/@/hooks/useEditor';
+
+import MoveAppPng from '/assets/images/move-app.png';
+
 import './styles.css';
 
 export function Install() {
-  const { version, isInstalled, isInstalling } = useEditor();
+  const { version, isInstalled, isInstalling, error } = useEditor();
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -17,10 +22,33 @@ export function Install() {
 
   return (
     <div className="Install">
-      <div className="loading">
-        <Loader />
-        <div className="message">{t('install.message', { version })}</div>
-      </div>
+      {error ? (
+        <Error error={error} />
+      ) : (
+        <div className="loading">
+          <Loader />
+          <div className="message">{t('install.message', { version })}</div>
+        </div>
+      )}
     </div>
+  );
+}
+
+function Error({ error }: { error: Error | string }) {
+  const errorNode = useMemo(() => {
+    if (error instanceof Error) return error.message;
+    if (error === 'MOVE_APP_FAILED') return <MoveAppFailed />;
+    return t('install.errors.unknown');
+  }, [error]);
+
+  return <div className="error">{errorNode}</div>;
+}
+
+function MoveAppFailed() {
+  return (
+    <>
+      <Typography variant="h5">{t('install.errors.moveAppFailed')}</Typography>
+      <img src={MoveAppPng} />
+    </>
   );
 }
