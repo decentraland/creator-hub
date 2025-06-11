4b1f9c1b0bc2f53c16e79e0311b2a2c5cd255304
alejandralevy <alelevy15@gmail.com>
2025-06-11 12:54:38 +0200

    feat: change updater to generate test build

--- packages/main/src/modules/updater.ts
+++ packages/main/src/modules/updater.ts
@@ -70,6 +70,10 @@ function configureUpdater(config: UpdaterConfig) {
   const { autoDownload } = config;
   updater.autoUpdater.autoDownload = autoDownload ?? false;
   updater.autoUpdater.autoInstallOnAppQuit = false;
+  updater.autoUpdater.forceDevUpdateConfig = true;
+  updater.autoUpdater.setFeedURL(
+    'https://github.com/decentraland/creator-hub/releases/download/0.14.2',
+  );
 }
 
 export async function checkForUpdates(config: UpdaterConfig = {}) {
