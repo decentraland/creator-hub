ce076239ba20b6259dd35799ff412e5f6f479bac
alejandralevy <alelevy15@gmail.com>
2025-06-25 12:59:45 +0200

    feat: update js-runtime when updating scene dependencies

--- packages/main/src/modules/npm.ts
+++ packages/main/src/modules/npm.ts
@@ -4,7 +4,7 @@ import { run, StreamError } from './bin';
 
 export async function install(path: string, packages: string[] = []) {
   const installCommand = run('npm', 'npm', {
-    args: ['install', '--loglevel', 'error', ...packages.flatMap(dep => ['--save', dep])],
+    args: ['install', '--loglevel', 'error', '--save-exact', ...packages],
     cwd: path,
   });
   await installCommand.wait();
