3e7cf6ec42fbcace70605b6613824dd4a8db81ff
Ignacio Mazzara <nachomazzara@gmail.com>
2025-07-28 10:35:05 -0300

    feat: remove canny
    Signed-off-by: Ignacio Mazzara <nachomazzara@gmail.com>

--- packages/renderer/src/modules/utils.ts
+++ packages/renderer/src/modules/utils.ts
@@ -4,5 +4,5 @@ export function isDev(chainId: ChainId): boolean {
   return chainId === ChainId.ETHEREUM_SEPOLIA;
 }
 
-export const REPORT_ISSUES_URL = 'https://decentraland.canny.io/creator-hub';
+export const REPORT_ISSUES_URL = 'https://decentraland.typeform.com/creatorhub';
 export const FEEDBACK_URL = 'https://decentraland.typeform.com/creatorhub';
