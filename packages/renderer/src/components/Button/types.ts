67ab752c57b62aa4db99bcfa981bb4ebb630751a
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-08-01 18:55:21 -0300

    feat: Add data-testid prop to components

--- packages/renderer/src/components/Button/types.ts
+++ packages/renderer/src/components/Button/types.ts
@@ -3,6 +3,7 @@ import type { ButtonOwnProps } from 'decentraland-ui2';
 export type ButtonProps = ButtonOwnProps & {
   className?: string;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  'data-testid'?: string;
 };
 
 export type GroupProps = ButtonProps & {
