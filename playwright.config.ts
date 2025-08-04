d5c4306cf4be2bf0493ea082bbc2d428b852b65a
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-08-04 10:55:52 -0300

    fix: Updates reporter to list

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,11 +15,13 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
   },
 
   /* Configure projects for major browsers */
