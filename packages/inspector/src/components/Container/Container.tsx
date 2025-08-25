de571a85ebcaae58935b2fc9de0fe9651580c9a8
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-08-25 17:15:32 -0300

    feat: Add a ContainerContent component to properly show tooltips from inside the Container component

--- packages/inspector/src/components/Container/Container.tsx
+++ packages/inspector/src/components/Container/Container.tsx
@@ -7,6 +7,18 @@ import { Button } from '../Button';
 import { InfoTooltip } from '../ui/InfoTooltip';
 import MoreOptionsMenu from '../EntityInspector/MoreOptionsMenu';
 
+/**
+ * ContainerContent is a wrapper component that allows returning both main content
+ * and right content from within a field renderer, while ensuring they become
+ * direct children of the Container.
+ */
+export const ContainerContent: React.FC<{
+  content: React.ReactNode;
+  rightContent?: React.ReactNode;
+}> = _ => {
+  return null;
+};
+
 import { type Props } from './types';
 
 import './Container.css';
@@ -14,6 +26,34 @@ import './Container.css';
 const Container: React.FC<React.PropsWithChildren<Props>> = props => {
   const [open, setOpen] = useState<boolean>(props.initialOpen ?? true);
   const Icon = open ? <IoIosArrowDown className="icon" /> : <IoIosArrowForward className="icon" />;
+
+  const { children, rightContentFromChildren } = useMemo(() => {
+    const childrenArray = React.Children.toArray(props.children);
+
+    // Find ContainerContent
+    const containerContentIndex = childrenArray.findIndex(
+      child => React.isValidElement(child) && child.type === ContainerContent,
+    );
+
+    // Handle ContainerContent (it can provide both content and right content)
+    if (containerContentIndex !== -1) {
+      const containerContent = childrenArray[containerContentIndex];
+      const otherChildren = childrenArray.filter((_, index) => index !== containerContentIndex);
+
+      if (React.isValidElement(containerContent)) {
+        const { content, rightContent } = containerContent.props;
+        return {
+          children: [...otherChildren, content],
+          rightContentFromChildren: rightContent || null,
+        };
+      }
+    }
+
+    return {
+      children: childrenArray,
+      rightContentFromChildren: null,
+    };
+  }, [props.children]);
 
   const renderIndicator = useCallback(() => {
     if (props.indicator) {
@@ -37,14 +77,16 @@ const Container: React.FC<React.PropsWithChildren<Props>> = props => {
     return null;
   }, [props.indicator]);
 
+  const finalRightContent = rightContentFromChildren || props.rightContent;
+
   const shouldRenderRightContent = useMemo(() => {
-    return props.rightContent || props.onRemoveContainer;
-  }, [props]);
+    return finalRightContent || props.onRemoveContainer;
+  }, [finalRightContent, props.onRemoveContainer]);
 
   const renderRightContent = useCallback(() => {
     return (
       <div className="RightContent">
-        {props.rightContent}
+        {finalRightContent}
         {props.onRemoveContainer && (
           <MoreOptionsMenu>
             <Button
@@ -57,7 +99,7 @@ const Container: React.FC<React.PropsWithChildren<Props>> = props => {
         )}
       </div>
     );
-  }, [props]);
+  }, [finalRightContent, props.onRemoveContainer]);
 
   return (
     <div
@@ -78,7 +120,7 @@ const Container: React.FC<React.PropsWithChildren<Props>> = props => {
           {shouldRenderRightContent && renderRightContent()}
         </div>
       )}
-      {open && <div className="content">{props.children}</div>}
+      {open && <div className="content">{children}</div>}
     </div>
   );
 };
