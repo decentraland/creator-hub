947a4e98fa96c913612c02b540c098069a355dda
alejandralevy <alelevy15@gmail.com>
2025-09-03 11:28:36 +0200

    check if tags are available

--- packages/inspector/src/components/EntityInspector/TagsInspector/TagsInspector.tsx
+++ packages/inspector/src/components/EntityInspector/TagsInspector/TagsInspector.tsx
@@ -12,12 +12,29 @@ export type Props = {
 
 const TagsInspector: React.FC<Props> = ({ entity }) => {
   const { getComponents } = useEntityComponent();
+
   const entityComponents = Array.from(getComponents(entity, false)).map(([id, name]) => ({
     id,
     name,
   }));
 
+  const allComponents = Array.from(getComponents(entity, true)).map(([id, name]) => ({
+    id,
+    name,
+  }));
+
+  const tagsComponents = allComponents.filter(component => component.name.startsWith('tag::'));
+  const entityTags = entityComponents.filter(component => component.name.startsWith('tag::'));
+
+  console.log('entityComponents', entityComponents);
+  console.log('availableComponents', allComponents);
+  console.log('tags', tagsComponents);
+
   const tags = ['Tag01', 'Tag02', 'Tag03'];
+
+  if (tagsComponents.length === 0) {
+    return null;
+  }
 
   return (
     <div className="TagsInspector">
