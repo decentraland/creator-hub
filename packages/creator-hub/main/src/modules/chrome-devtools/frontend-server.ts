08ff9b90c40978c37ea5c75fbd2e796d5b634924
nickkhalow <nickhalow@gmail.com>
2025-09-09 18:41:18 +0300

    fix deps

--- packages/creator-hub/main/src/modules/chrome-devtools/frontend-server.ts
+++ packages/creator-hub/main/src/modules/chrome-devtools/frontend-server.ts
@@ -1,10 +1,13 @@
-import { type Server as HttpServer } from 'node:http';
-import * as path from 'path';
+import { once } from 'node:events';
+
+import type { Server as NetServer } from 'node:net';
+
+import log from 'electron-log/main';
 
 import type { Result } from 'ts-results-es';
 import { Ok, Err } from 'ts-results-es';
-import type { Express, NextFunction, Request, Response } from 'express';
-import express from 'express';
+
+import { createServer } from 'http-server';
 
 import { getAvailablePort } from '../port';
 import { type ServerPort } from './client';
@@ -20,10 +23,8 @@ export type ChromeDevToolsFrontendServer = {
 export function newChromeDevToolsFrontendServer(
   staticServerDirPath: string,
 ): ChromeDevToolsFrontendServer {
-  const expressAppInstance: Express = express();
-
   let runningServerPort: ServerPort | null = null;
-  let serverInstance: HttpServer | null = null;
+  let serverInstance: NetServer | null = null;
 
   function port(): Result<ServerPort, string> {
     if (runningServerPort === null) {
@@ -42,25 +43,15 @@ export function newChromeDevToolsFrontendServer(
       return new Err('Server is already running');
     }
 
-    expressAppInstance.use((_req: Request, res: Response, next: NextFunction) => {
-      res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-      res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-      next();
+    const port = await getAvailablePort();
+
+    serverInstance = createServer({ root: staticServerDirPath });
+    serverInstance.listen(port, () => {
+      log.info(`DevTools server running at http://localhost:${port}`);
     });
 
-    expressAppInstance.use(
-      express.static(staticServerDirPath, { immutable: true, maxAge: '1h', index: 'index.html' }),
-    );
+    await once(serverInstance, 'listening');
 
-    // SPA fallback
-    expressAppInstance.use((_req: Request, res: Response) =>
-      res.sendFile(path.join(staticServerDirPath, 'index.html')),
-    );
-
-    const port = await getAvailablePort();
-    serverInstance = expressAppInstance.listen(port, '127.0.0.1');
-    const serverReferenceCapture = serverInstance;
-    await new Promise<void>(r => serverReferenceCapture.once('listening', () => r()));
     runningServerPort = { port };
     return new Ok(runningServerPort);
   }
