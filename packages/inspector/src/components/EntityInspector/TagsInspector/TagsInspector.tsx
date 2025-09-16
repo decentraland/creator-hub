945b9fa39f56a1a6483442378b38f6f0af1226b5
alejandralevy <alelevy15@gmail.com>
2025-09-16 12:22:26 +0200

    feat: add hook to update values

--- packages/inspector/src/components/EntityInspector/TagsInspector/TagsInspector.tsx
+++ packages/inspector/src/components/EntityInspector/TagsInspector/TagsInspector.tsx
@@ -1,23 +1,25 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import './TagsInspector.css';
 import { FaTag as TagIcon, FaPlus } from 'react-icons/fa';
 import type { Entity } from '@dcl/ecs';
 
 import { withSdk } from '../../../hoc/withSdk';
 import { Dropdown, type DropdownChangeEvent } from '../../ui/Dropdown';
-import {
-  getSceneTags,
-  getTagsForEntity,
-  updateTagsForEntity,
-} from '../../../lib/sdk/components/Tags';
+import { getTagComponent, updateTagsForEntity } from '../../../lib/sdk/components/Tags';
+import { useComponentValue } from '../../../hooks/sdk/useComponentValue';
 import { CreateEditTagModal } from './CreateEditTagModal';
 
 const TagsInspector = withSdk<{ entity: Entity }>(({ entity, sdk }) => {
   const [open, setOpen] = useState(false);
 
-  const tags = useMemo(() => getSceneTags(sdk.engine), [entity]);
+  const [sceneTagsComponent] = useComponentValue(
+    sdk.engine.RootEntity,
+    getTagComponent(sdk.engine),
+  );
+  const [entityTagsComponent] = useComponentValue(entity, getTagComponent(sdk.engine));
 
-  const entityTags = useMemo(() => getTagsForEntity(sdk.engine, entity), [entity, sdk]);
+  const sceneTags = sceneTagsComponent?.tags || [];
+  const entityTags = entityTagsComponent?.tags || [];
 
   const handleCreateNewTag = (e: React.ChangeEvent<HTMLSelectElement>) => {
     e.preventDefault();
@@ -26,10 +28,10 @@ const TagsInspector = withSdk<{ entity: Entity }>(({ entity, sdk }) => {
 
   const handleTagChange = useCallback(
     ({ target: { value } }: DropdownChangeEvent) => {
-      const selectedTags = tags.filter(tag => value.includes(tag.name));
+      const selectedTags = sceneTags.filter(tag => value.includes(tag.name));
       updateTagsForEntity(sdk.engine, entity, selectedTags);
     },
-    [entity, sdk, tags],
+    [entity, sdk, sceneTags],
   );
 
   return (
@@ -44,7 +46,7 @@ const TagsInspector = withSdk<{ entity: Entity }>(({ entity, sdk }) => {
           onChange={handleTagChange}
           value={entityTags.map(tag => tag.name)}
           options={[
-            ...tags.map(tag => ({ label: tag.name, value: tag.name })),
+            ...sceneTags.map(tag => ({ label: tag.name, value: tag.name })),
             {
               label: 'Create a new tag',
               value: 'create',
