7ee51cccdc25f33fff74b7c0af05f7974f623037
Rocío Mena <69587750+RocioCM@users.noreply.github.com>
2025-09-17 12:36:20 -0300

    Fix: offer deleting files when deleting project (#787)

--- packages/creator-hub/renderer/src/components/Modals/DeleteProject/component.tsx
+++ packages/creator-hub/renderer/src/components/Modals/DeleteProject/component.tsx
@@ -1,5 +1,6 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
 
+import { Box, Checkbox } from 'decentraland-ui2';
 import { t } from '/@/modules/store/translation/utils';
 
 import { Modal } from '..';
@@ -7,17 +8,26 @@ import { Button } from '../../Button';
 
 import type { Props } from './types';
 
+import './styles.css';
+
 export function DeleteProject({ open, project, onClose, onSubmit }: Props) {
+  const [shouldDeleteFiles, setShouldDeleteFiles] = useState(false);
+
+  const handleCheckboxChange = useCallback(
+    (_event: React.ChangeEvent<HTMLInputElement>, checked: boolean) =>
+      setShouldDeleteFiles(checked),
+    [],
+  );
   const handleSubmit = useCallback(() => {
-    onSubmit(project);
-  }, []);
+    onSubmit(project, shouldDeleteFiles);
+  }, [shouldDeleteFiles, onSubmit, project]);
 
   return (
     <Modal
       open={open}
       title={t('modal.delete_project.title', { title: project.title })}
-      onClose={onClose}
       size="tiny"
+      className="DeleteProjectModal"
       actions={
         <>
           <Button
@@ -30,7 +40,17 @@ export function DeleteProject({ open, project, onClose, onSubmit }: Props) {
         </>
       }
     >
-      {t('modal.delete_project.remove_imported_scene', { title: project.title })}
+      <label className="delete-files">
+        <Checkbox
+          value={shouldDeleteFiles}
+          onChange={handleCheckboxChange}
+        />
+        <span>{t('modal.delete_project.files_checkbox')}</span>
+      </label>
+
+      {shouldDeleteFiles && (
+        <p className="delete-files-warning">{t('modal.delete_project.files_warning')}</p>
+      )}
     </Modal>
   );
 }
