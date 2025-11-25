95733dd6fe86cd8313ae2e29bd957440f84e7fd0
Rocío Mena <69587750+RocioCM@users.noreply.github.com>
2025-11-25 15:06:42 -0300

    Enhance unexpected error handling while loading scenes (#921)

--- packages/creator-hub/renderer/src/modules/store/editor/slice.ts
+++ packages/creator-hub/renderer/src/modules/store/editor/slice.ts
@@ -1,9 +1,10 @@
 import { createSlice, isRejectedWithValue } from '@reduxjs/toolkit';
+import { captureException } from '@sentry/electron/renderer';
 
 import { createAsyncThunk } from '/@/modules/store/thunk';
 
 import type { DeployOptions } from '/shared/types/deploy';
-import { ProjectError, type Project } from '/shared/types/projects';
+import { isProjectError, ProjectError, type Project } from '/shared/types/projects';
 import type { PreviewOptions } from '/shared/types/settings';
 import { isWorkspaceError } from '/shared/types/workspace';
 
@@ -95,7 +96,12 @@ export const slice = createSlice({
       if (isRejectedWithValue(action) && isWorkspaceError(action.payload, 'PROJECT_NOT_FOUND')) {
         state.error = action.payload;
       } else {
-        state.error = action.error.message || 'Failed to run project';
+        state.error = new ProjectError('FAILED_TO_RUN_PROJECT');
+
+        captureException(state.error, {
+          tags: { source: 'editor-page' },
+          extra: { context: 'Unknown error in runProject', action },
+        });
       }
       state.project = undefined;
     });
@@ -132,6 +138,10 @@ export const slice = createSlice({
     });
     builder.addCase(workspaceActions.createProject.rejected, state => {
       state.error = new ProjectError('PROJECT_NOT_CREATED');
+      state.project = undefined;
+    });
+    builder.addCase(workspaceActions.createProjectAndInstall.rejected, state => {
+      if (isProjectError(state.error)) state.error = new ProjectError('PROJECT_NOT_CREATED');
       state.project = undefined;
     });
     builder.addCase(workspaceActions.updateProject, (state, action) => {
@@ -179,6 +189,7 @@ export const slice = createSlice({
       state.isInstallingProject = false;
     });
     builder.addCase(workspaceActions.installProject.rejected, state => {
+      state.error = new ProjectError('FAILED_TO_INSTALL_DEPENDENCIES');
       state.isInstallingProject = false;
     });
     builder.addCase(workspaceActions.saveAndGetThumbnail.fulfilled, (state, action) => {
