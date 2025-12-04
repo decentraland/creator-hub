5b2de1aa8e45f08a21e36873ff3d16bc241bd481
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-12-04 17:46:21 -0300

    feat: Edit Material component with multiples entities

--- packages/inspector/src/components/EntityInspector/EntityInspector.tsx
+++ packages/inspector/src/components/EntityInspector/EntityInspector.tsx
@@ -65,6 +65,7 @@ const MultiEntityInspector = withSdk<{ entities: Entity[] }>(({ sdk, entities })
     () => [
       { name: sdk.components.Tags.componentName, component: TagsInspector },
       { name: sdk.components.Transform.componentName, component: TransformInspector },
+      { name: sdk.components.Material.componentName, component: MaterialInspector },
     ],
     [sdk],
   );
@@ -200,6 +201,7 @@ const SingleEntityInspector = withSdk<{ entity: Entity | null }>(({ sdk, entity 
                 <Inspector
                   key={`${index}-${entity}`}
                   entity={entity}
+                  entities={[entity]}
                   initialOpen={!isBasicViewEnabled}
                 />
               ),
