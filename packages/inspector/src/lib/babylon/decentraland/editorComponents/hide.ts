0caa9b07051a8be8a44b69f586712948a5535eea
Rocío <69587750+RocioCM@users.noreply.github.com>
2025-12-17 16:41:13 -0300

    Fix: Hidden items don't stay invisible (#974)

--- packages/inspector/src/lib/babylon/decentraland/editorComponents/hide.ts
+++ packages/inspector/src/lib/babylon/decentraland/editorComponents/hide.ts
@@ -3,19 +3,22 @@ import type { ComponentOperation } from '../component-operations';
 import { setGizmoManager, unsetGizmoManager } from './selection';
 
 export const putHideComponent: ComponentOperation = (entity, component) => {
-  const container = entity.gltfContainer ?? entity.meshRenderer;
-  if (!container) return;
+  // Ensure asset is loaded before applying hide.
+  entity.onAssetLoaded().then(() => {
+    const container = entity.gltfContainer ?? entity.meshRenderer;
+    if (!container) return;
 
-  if (component.componentType === ComponentType.LastWriteWinElementSet) {
-    const context = entity.context.deref()!;
-    const { value: isHidden } =
-      (component.getOrNull(entity.entityId) as { value: boolean } | null) ?? {};
-    container.setEnabled(!isHidden);
-    if (isHidden) {
-      unsetGizmoManager(entity);
-    } else {
-      const selectionValue = context.editorComponents.Selection.getOrNull(entity.entityId);
-      if (selectionValue) setGizmoManager(entity, selectionValue);
+    if (component.componentType === ComponentType.LastWriteWinElementSet) {
+      const context = entity.context.deref()!;
+      const { value: isHidden } =
+        (component.getOrNull(entity.entityId) as { value: boolean } | null) ?? {};
+      container.setEnabled(!isHidden);
+      if (isHidden) {
+        unsetGizmoManager(entity);
+      } else {
+        const selectionValue = context.editorComponents.Selection.getOrNull(entity.entityId);
+        if (selectionValue) setGizmoManager(entity, selectionValue);
+      }
     }
-  }
+  });
 };
