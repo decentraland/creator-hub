cde1e8dfaef1a2d2f7160aaf7abcb62cb9a951dd
Nicolas Echezarreta <nicoecheza@gmail.com>
2025-12-19 11:15:50 -0300

    fix: dont close publish modal on backdrop click (#982)

--- packages/creator-hub/renderer/src/components/Modals/PublishProject/PublishModal/component.tsx
+++ packages/creator-hub/renderer/src/components/Modals/PublishProject/PublishModal/component.tsx
@@ -3,12 +3,22 @@ import { type ModalProps } from 'decentraland-ui2/dist/components/Modal/Modal.ty
 import { Modal, onBackNoop } from '../..';
 
 export function PublishModal(props: React.PropsWithChildren<ModalProps>) {
-  const { onBack, ...rest } = props;
+  const { onBack, onClose, ...rest } = props;
+
+  const handleClose = (
+    event: React.MouseEvent<HTMLButtonElement>,
+    reason?: 'backdropClick' | 'escapeKeyDown',
+  ) => {
+    if (reason === 'backdropClick') return;
+    onClose?.(event);
+  };
+
   return (
     <Modal
       size="small"
       {...rest}
       onBack={onBack || onBackNoop}
+      onClose={handleClose as any} // ui2 is not typing this correctly..."reason" is being provided anyway...
     >
       {props.children}
     </Modal>
