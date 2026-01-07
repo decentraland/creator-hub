af31045387d77028463e32aa507a0814040726ca
Nicolas Earnshaw <earnshaw.nico@gmail.com>
2026-01-07 17:28:21 -0300

    Update templates.ts
    Signed-off-by: Nicolas Earnshaw <earnshaw.nico@gmail.com>

--- packages/inspector/src/components/EntityInspector/ScriptInspector/templates.ts
+++ packages/inspector/src/components/EntityInspector/ScriptInspector/templates.ts
@@ -4,15 +4,33 @@ export function getScriptTemplateClass(scriptName: string): string {
   const className = toPascalCase(scriptName, 'Script') || 'Script';
   return `
 import { engine, Entity } from '@dcl/sdk/ecs'
+// Add any additional import statements above
 
 export class ${className} {
+  /**
+   * Properties
+   * Define class fields you want to reuse across methods.
+   * Example usage: this.myVariable
+   */
+   // private myVariable: boolean = true
+  
+  /**
+   * Constructor / Inputs
+   * Parameters declared here appear in the Script component UI in Creator Hub.
+   * Supported types: Entity, String, Number, Boolean.
+   *
+   * Note: After editing this file, click the refresh icon in the Script component UI
+   * to see updated inputs.
+   */
   constructor(
-    public src: string,
-    public entity: Entity
+    public src: string,     // DO NOT REMOVE
+    public entity: Entity,   // DO NOT REMOVE
+    // Add your custom inputs below
   ) {}
 
   /**
-   * Start function - called when the script is initialized
+   * start()
+   * Called once when the script is initialized.
    */
   start() {
     // Script initialization
@@ -20,8 +38,9 @@ export class ${className} {
   }
 
   /**
-   * Update function - called every frame
-   * @param dt - Delta time since last frame (in seconds)
+   * update(dt)
+   * Called every frame.
+   * @param dt - (optional) Delta time since last frame (in seconds)
    */
   update(dt: number) {
     // Called every frame
