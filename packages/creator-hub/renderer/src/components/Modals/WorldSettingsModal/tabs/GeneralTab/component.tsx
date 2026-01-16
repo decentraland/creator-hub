2dda21a28b3d394a705621e7450d87553f43def4
Rocío Mena <rociocmena@gmail.com>
2026-01-16 10:50:09 -0300

    Add world settings general tab with spawn coords UI

--- packages/creator-hub/renderer/src/components/Modals/WorldSettingsModal/tabs/GeneralTab/component.tsx
+++ packages/creator-hub/renderer/src/components/Modals/WorldSettingsModal/tabs/GeneralTab/component.tsx
@@ -1,25 +1,19 @@
 import React, { useCallback } from 'react';
-import {
-  Box,
-  Checkbox,
-  FormControlLabel,
-  FormGroup,
-  OutlinedInput,
-  Typography,
-} from 'decentraland-ui2';
+import { Box, FormGroup, OutlinedInput, Typography } from 'decentraland-ui2';
 import { t } from '/@/modules/store/translation/utils';
-import { MAX_COORDINATE, MIN_COORDINATE } from '/@/modules/world';
 import type { WorldSettings } from '/@/lib/worlds';
-import { coordsToId, idToCoords } from '/@/lib/land';
 import { Row } from '/@/components/Row';
-import { RangeHourField, MIDDAY_SECONDS } from '/@/components/RangeHourField';
 import './styles.css';
 
-const INTEGER_REGEX = /^-?\d+$/;
+/// TODO: get real numbers from backenders
+const MAX_COORDINATE = 10000;
+const MIN_COORDINATE = -10000;
+
+const formatSpawnPoint = (x: string, y: string) => `${x},${y}`;
 
 type Props = {
   worldSettings: WorldSettings;
-  onChangeSettings: (settings: Partial<WorldSettings>) => void;
+  onChangeSpawnPoint: (spawnPoint: string) => void;
 };
 
 const TitleDivider = ({ title }: { title: string }) => (
@@ -29,13 +23,13 @@ const TitleDivider = ({ title }: { title: string }) => (
   </Typography>
 );
 
-const GeneralTab: React.FC<Props> = ({ worldSettings, onChangeSettings }) => {
-  const [x, y] = idToCoords(worldSettings.spawnCoordinates || '');
-  const isSkyboxAuto = worldSettings.skyboxTime === undefined || worldSettings.skyboxTime === null;
+const GeneralTab: React.FC<Props> = ({ worldSettings, onChangeSpawnPoint }) => {
+  const [x = '', y = ''] = worldSettings.spawnCoordinates?.split(',') || [];
 
   const validateCoordinate = useCallback((value: string) => {
-    if (value === '') return true;
-    if (!INTEGER_REGEX.test(value)) return false;
+    const integerRegex = /^-?\d+$/;
+    if (!integerRegex.test(value)) return false;
+
     const numValue = parseInt(value, 10);
     return !isNaN(numValue) && numValue >= MIN_COORDINATE && numValue <= MAX_COORDINATE;
   }, []);
@@ -43,15 +37,15 @@ const GeneralTab: React.FC<Props> = ({ worldSettings, onChangeSettings }) => {
   return (
     <Box className="GeneralTab">
       <FormGroup>
-        <TitleDivider title={t('modal.world_settings.general.world_spawn_coordinate')} />
+        <TitleDivider title={t('modal.world_settings.tabs.general.world_spawn_coordinate')} />
         <Box className="InputContainer">
-          <Typography variant="body2">{t('modal.world_settings.general.position')}</Typography>
+          <Typography>{t('modal.world_settings.tabs.general.position')}</Typography>
           <Row>
             <OutlinedInput
               value={x}
               onChange={e => {
                 if (validateCoordinate(e.target.value)) {
-                  onChangeSettings({ spawnCoordinates: coordsToId(e.target.value, y) });
+                  onChangeSpawnPoint(formatSpawnPoint(e.target.value, y));
                 }
               }}
               startAdornment={<span className="Label">X</span>}
@@ -60,40 +54,13 @@ const GeneralTab: React.FC<Props> = ({ worldSettings, onChangeSettings }) => {
               value={y}
               onChange={e => {
                 if (validateCoordinate(e.target.value)) {
-                  onChangeSettings({ spawnCoordinates: coordsToId(x, e.target.value) });
+                  onChangeSpawnPoint(formatSpawnPoint(x, e.target.value));
                 }
               }}
               startAdornment={<span className="Label">Y</span>}
             />
           </Row>
         </Box>
-      </FormGroup>
-      <FormGroup>
-        <TitleDivider title={t('modal.world_settings.general.world_skybox')} />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={isSkyboxAuto}
-              onChange={e =>
-                onChangeSettings({ skyboxTime: e.target.checked ? null : MIDDAY_SECONDS })
-              }
-            />
-          }
-          label={t('modal.world_settings.general.auto_skybox')}
-        />
-        <Typography variant="body2">{t('modal.world_settings.general.max_offset')}</Typography>
-        <RangeHourField
-          value={worldSettings.skyboxTime ?? MIDDAY_SECONDS}
-          disabled={isSkyboxAuto}
-          onChange={e => onChangeSettings({ skyboxTime: parseInt(e.target.value) })}
-        />
-      </FormGroup>
-      <FormGroup>
-        <TitleDivider title={t('modal.world_settings.general.general')} />
-        <FormControlLabel
-          control={<Checkbox />}
-          label={t('modal.world_settings.general.single_player')}
-        />
       </FormGroup>
     </Box>
   );
