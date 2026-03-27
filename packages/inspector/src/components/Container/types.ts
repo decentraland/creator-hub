e6f764235d5b33e0d9dda45efd9f490783dbb31b
Kevin Decibe <kevindecibe@gmail.com>
2026-03-27 17:56:41 -0300

    hasEntityInList helper added, indicatorSeverity prop added in Container component

--- packages/inspector/src/components/Container/types.ts
+++ packages/inspector/src/components/Container/types.ts
@@ -1,9 +1,12 @@
+export type IndicatorSeverity = 'warning' | 'error';
+
 export type Props = {
   label?: string;
   className?: string;
   rightContent?: JSX.Element;
   initialOpen?: boolean;
   indicator?: boolean | string | JSX.Element;
+  indicatorSeverity?: IndicatorSeverity;
   border?: boolean;
   gap?: boolean;
   variant?: 'minimal';
